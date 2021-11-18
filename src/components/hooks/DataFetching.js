056ee99763e9ccaa12d83f339b54cc4d77d6ca23
rutuja <rutujaingale0230@example.com>
2021-11-18 11:49:16 +0530

    getting only one post

--- src/components/hooks/DataFetching.js
+++ src/components/hooks/DataFetching.js
@@ -1,34 +1,45 @@
-
 import React, {useState,useEffect} from 'react'
 import axios from 'axios'
 
 function DataFetching() {
-    const [posts,setPosts] = useState([])
+    const [post,setPost] = useState({})
+    const [id,setId] = useState(1)
 
-    useEffect(
+    /**
+     * ///useEffect is about the lifecycle methods of a class component --
+@@ -11,27 +12,33 @@ function DataFetching() {
+    useEffect(    
+        //the arrow function below will get executed after every render
         () => {
-            axios.get('https://jsonplaceholder.typicode.com/posts')
+            axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then( res => {
                 console.log(res)
-                setPosts(res.data)
+                 setPost(res.data)
             })
             .catch(err => {
                 console.log(err)
             })
-        }
+        },
+        [id] //run this useEffect hook only once ie after componentDidMount
     )
 
 
 
     return (
         <div>
-            <ul>{
+         <input 
+            type = "text"
+            value = {id}
+            onChange = { e => setId(e.target.value)}
+            />
+            <div>{post.title}</div>
+           {/* <ul>{
                 posts.map(
                     post => <li key = {post.id}>{post.title}</li>
                 )
-            }</ul>
+                  }</ul>*/}
         </div>
-    )
-}
+        )
+                }
 
-export default DataFetching
+    export default DataFetching
